323717191ab867a255d66e55602865f001e339ea
zbww <zhoubw12@mails.tsinghua.edu.cn>
2014-07-11 10:44:18 +0800

    no message

--- fe/js/main.js
+++ fe/js/main.js
@@ -1,9 +1,11 @@
-var p;
-var now=0;
-var settime=true;
-var timer;
-var datasrc="data/news.json"; 
+/**************************************************下面设置一些初值***************************************************/
+var p;//图片新闻的json对象
+var now=(window.localStorage.now==undefined?(window.localStorage.now=0):window.localStorage.now);//当前图片位置
+var settime=true;//表示当前已经设置计时器
+var timer;//计时器
+var datasrc="data/news.json"; //json文件路径
 var t=3500;//设置时间间隔
+/**************************************************加载图片新闻信息***************************************************/
 $('body')[0].innerHTML='';
 q=$.getJSON(datasrc, function(data){ 
 p=data;
@@ -11,9 +13,12 @@ printJSON(p);
 addInfo(p);
 addImg(p);
 timer=setInterval(function(){moveright();},t);
+for (var i=0;i<now;i++)//使用localStorage，把图片翻到合适位置
+{
+$('.pic'+i).fadeOut(0);
+}
 }); 
-//p:获取来的json对象
-//q:get到的信息
+/**************************************************打印获取到的json信息***************************************************/
 function printJSON(p)//打印JSON信息
 {
 	var viewJSON=
@@ -50,9 +55,10 @@ function printJSON(p)//打印JSON信息
 	viewJSON+='</div>';
 	$('body').prepend(viewJSON);
 }
+/****************************************************把图像放上去*************************************************/
 function addImg(p)
 {
-	var imgArea='<div id="imgArea" style="'+
+	var imgArea='<div id="imgArea" style="'+         //这是图像的区域
 		'box-shadow: 5px 5px 14px #888888;'+
 		'border-radius: 7px;'+
 	//	'border-bottom-left-radius: 9px;'+
@@ -65,7 +71,7 @@ function addImg(p)
 		'position:absolute;'+
 		//'float:left;'+
 		'">';
-	for (var i=p.num-1;i>=0;i--)
+	for (var i=p.num-1;i>=0;i--)					//逐张添加图像
 	{
 		imgArea+='<div style="'+
 		'position:absolute;'+
@@ -80,14 +86,15 @@ function addImg(p)
 		'" width=654px height=420px></div>';
 	}
 	imgArea+='</div>';
-	imgArea+=
+	imgArea+=										//两个箭头按钮
 	'<div id="left-arrow" class="arrow" style="position:absolute;top:191px;left:29px;height:100px;background:#eee;opacity:0;border-bottom-right-radius: 7px;border-top-right-radius: 7px;"><img id="left-arrow" class="arrow" src="img/arrow-left.png"></div>'+
 	'<div id="right-arrow" class="arrow" style="position:absolute;top:191px;left:652px;height:100px;background:#eee;opacity:0;border-bottom-left-radius: 7px;border-top-left-radius: 7px;"><img id="right-arrow" class="arrow" src="img/arrow-right.png"></div>';
 	$('body').prepend(imgArea);
 }
+/***************************************************添加图片上的新闻**************************************************/
 function addInfo(p)
 {
-	var info='<div id="infoArea" class="info" style="'+
+	var info='<div id="infoArea" class="info" style="'+//新闻区域
 		'box-shadow: 5px 5px 14px #888888;'+
 		//'border-top-right-radius: 9px;'+
 	//	'border-bottom-right-radius: 5px;'+
@@ -100,7 +107,7 @@ function addInfo(p)
 		'margin-top:297px;'+
 		'opacity:0;'+
 	'">';
-	for (var i=0;i<p.num;i++)
+	for (var i=0;i<p.num;i++)							//逐条添加新闻
 	{
 		info+='<div class="info" id="info'+
 		i+
@@ -128,8 +135,8 @@ function addInfo(p)
 	info+='</div>';
 	info+='<div style="height:460px"></div>';
 	$('body').prepend(info);
-	for (var i=1;i<6;i++)
-		$('#info'+i).fadeOut(0);
+	for (var i=0;i<6;i++)//隐藏其他新闻
+		if (i!=now) $('#info'+i).fadeOut(0);
 }
 /*
 @-webkit-keyframes awareru
@@ -143,18 +150,22 @@ from {opacity: 1;}
 to {opacity: 0;}
 }
 */
+/***************************************************图片、新闻翻页**************************************************/
+
 function moveleft()
 {
-	console.log(now);
+	//console.log(now);
 	$('#info'+now).fadeOut('slow');
 	if (now>0)
 	{
 		$('.pic'+(now-1)).fadeIn('slow');
 		now--;
+		window.localStorage.now--;
 	}
 	else
 	{
 		now=5;
+		window.localStorage.now=5;
 		for (i=0;i<5;i++) 
 		{
 			$('.pic'+i).fadeOut('slow');
@@ -170,10 +181,12 @@ function moveright()
 	{
 		$('.pic'+now).fadeOut('slow');
 		now++;
+		window.localStorage.now++;
 	}
 	else
 	{
 		now=0;
+		window.localStorage.now=0;
 		for (i=0;i<5;i++) 
 		{
 			$('.pic'+i).fadeIn('slow');
@@ -181,6 +194,7 @@ function moveright()
 	}
 	$('#info'+now).fadeIn('slow');
 }
+/***************************************************事件监听**************************************************/
 document.addEventListener('click',function(c)
 {
 	//console.log(c);
@@ -204,6 +218,7 @@ document.addEventListener('click',function(c)
 		if (page<2)
 		{
 			comment(++page);
+			window.localStorage.page++;
 		}
 		else
 		{
@@ -215,6 +230,7 @@ document.addEventListener('click',function(c)
 		if (page>0)
 		{
 			comment(--page);
+			window.localStorage.page--;
 		}
 		else
 		{
@@ -258,7 +274,7 @@ function min(a,b){return a<b?a:b;}
 function max(a,b){return a>b?a:b;}
 function dis(x1,y1,x2,y2){return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);}
 
-document.addEventListener('mousemove',function(e)
+document.addEventListener('mousemove',function(e)//控制两个箭头显示、隐藏
 {	
 	var x=e.x;
 	var y=e.y;
@@ -268,8 +284,8 @@ document.addEventListener('mousemove',function(e)
 	$('#left-arrow').css('opacity',max(0,(100000-d1)/150000));
 });
 
-/**************************************上面是图片部分，下面是评论部分*******************************************/
-var page=0;
+/**************************************下面是评论部分*******************************************/
+var page=(window.localStorage.page==undefined?(window.localStorage.page=0):window.localStorage.page);
 
 $('body').prepend('<div id="commentArea" style="'+
 'left:700px;'+
@@ -324,7 +340,7 @@ function addComment(s)
 	$('#comment')[0].innerHTML=c;
 }
 
-function randomComment(s)
+function randomComment(s)//生成随机评论
 {
 	var r=[];
 	for (var i=0;i<4;i++)
@@ -337,9 +353,9 @@ function randomComment(s)
 
 comment(page);
 
-
-$('body').prepend('<div class="s-skin-container" style="background-color:rgb(64, 64, 64);background-image:\'img/125.jpg\';"></div>')
-
+$('body').attr('background','img/125.jpg');
+$('body').css('background-size','100%');
+$('body').css('background-attachment','fixed');
 
 
 
